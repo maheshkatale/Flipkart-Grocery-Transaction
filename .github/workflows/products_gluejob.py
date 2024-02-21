13f5188b89728cd53898f04fd696860398100ea7
maheshkatale <148619752+maheshkatale@users.noreply.github.com>
2024-02-21 13:42:05 +0530

    Update products_gluejob.py

--- .github/workflows/products_gluejob.py
+++ .github/workflows/products_gluejob.py
@@ -5,7 +5,7 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from pyspark.sql.functions import when, col
-from awsglue.dynamicframe import DynamicFrame  # Add this import statement
+from awsglue.dynamicframe import DynamicFrame 
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -15,30 +15,30 @@ job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
 # Script generated for node Relational DB
-RelationalDB_node1707473645992 = glueContext.create_dynamic_frame.from_options(
+productsDF = glueContext.create_dynamic_frame.from_options(
     connection_type="mysql",
     connection_options={
         "useConnectionProperties": "true",
         "dbtable": "product",
         "connectionName": "Jdbc connection",
     },
-    transformation_ctx="RelationalDB_node1707473645992",
+    transformation_ctx="productsDF",
     additional_options={"jobBookmarkKeys": ["indexing"], "jobBookmarkKeysSortOrder": "asc"}
 )
 
 # Convert dynamic frame to DataFrame
-df = RelationalDB_node1707473645992.toDF()
+df = productsDF.toDF()
 
 # Fill missing values in DataFrame
-df = df.withColumn("brand_name", when(col("brand_name") == "", "UNKNOWN").otherwise(col("brand_name")))
-df = df.withColumn("manufacturer_name", when(col("manufacturer_name") == "", "LOCAL").otherwise(col("manufacturer_name")))
+df = df.withColumn("brand_name", when(col("brand_name") == "", "Unknown").otherwise(col("brand_name")))
+df = df.withColumn("manufacturer_name", when(col("manufacturer_name") == "", "Local").otherwise(col("manufacturer_name")))
 
 # Convert DataFrame back to dynamic frame
 dynamic_frame_filled = DynamicFrame.fromDF(df, glueContext, "dynamic_frame_filled")
 
 
 # Script generated for node Change Schema
-ChangeSchema_node1707477382758 = ApplyMapping.apply(
+productDF = ApplyMapping.apply(
     frame=dynamic_frame_filled,
     mappings=[
         ("product_id", "int", "product_id", "int"),
@@ -54,22 +54,22 @@ ChangeSchema_node1707477382758 = ApplyMapping.apply(
         ("l1_category_id", "int", "l1_category_id", "int"),
         ("l2_category_id", "int", "l2_category_id", "int"),
     ],
-    transformation_ctx="ChangeSchema_node1707477382758",
+    transformation_ctx="productDF",
 )
 
 
 # Script generated for node Amazon S3
-AmazonS3_node1708110424343 = glueContext.getSink(
-    path="s3://flipkart-project-input/product_output/",
+product_data_sink = glueContext.getSink(
+    path="s3://flipkart-outputs/product_output/",
     connection_type="s3",
     updateBehavior="LOG",
     partitionKeys=[],
     enableUpdateCatalog=True,
-    transformation_ctx="AmazonS3_node1708110424343",
+    transformation_ctx="product_data_sink",
 )
-AmazonS3_node1708110424343.setCatalogInfo(
-    catalogDatabase="flipkart_database", catalogTableName="products"
+product_data_sink.setCatalogInfo(
+    catalogDatabase="flipkart_database", catalogTableName="product"
 )
-AmazonS3_node1708110424343.setFormat("glueparquet", compression="uncompressed")
-AmazonS3_node1708110424343.writeFrame(ChangeSchema_node1707477382758)
+product_data_sink.setFormat("glueparquet", compression="uncompressed")
+product_data_sink.writeFrame(productDF)
 job.commit()
